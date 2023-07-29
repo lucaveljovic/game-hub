321f438354277464f8bfbbd92ab8c2aea60487b5
Luca Veljovic <luca.veljvic@gmail.com>
2023-07-29 18:23:42 +0200

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,7 +15,8 @@ const useGames = (gameQuery : GameQuery) =>
   useData<Game>("/games", {
     params: {
       genres: gameQuery.genre?.id, 
-      parent_platforms: gameQuery.platform?.id
+      parent_platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder
     } }, [gameQuery]);
 
 export default useGames;
